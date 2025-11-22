ac9a2270a52604f4d698e87bfda129f7c1a565db
PeriodicBrake63 <125907242+PeriodicBrake63@users.noreply.github.com>
2025-11-22 13:46:00 +0000

    none

--- src/main/java/com/PeriodicBrake_dev/FullCustomEvents/FullCustomEvents.java
+++ src/main/java/com/PeriodicBrake_dev/FullCustomEvents/FullCustomEvents.java
@@ -1,6 +1,9 @@
-package com.example.examplemod;
+package com.PeriodicBrake_dev.FullCustomEvents;
+
+import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.food.FoodProperties;
@@ -26,11 +29,10 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
+@Mod(FullCustomEvents.MODID)
+public class FullCustomEvents
 {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "examplemod";
@@ -60,7 +62,7 @@ public class ExampleMod
                 output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
             }).build());
 
-    public ExampleMod(FMLJavaModLoadingContext context)
+    public FullCustomEvents(FMLJavaModLoadingContext context)
     {
         IEventBus modEventBus = context.getModEventBus();
 
