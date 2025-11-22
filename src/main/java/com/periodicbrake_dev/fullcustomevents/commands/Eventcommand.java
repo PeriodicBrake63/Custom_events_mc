ca76fe46c921a62c1975e2737a3c4234cf1fe45e
PeriodicBrake63 <125907242+PeriodicBrake63@users.noreply.github.com>
2025-11-22 17:05:48 +0000

    none

--- src/main/java/com/periodicbrake_dev/fullcustomevents/commands/Eventcommand.java
+++ src/main/java/com/periodicbrake_dev/fullcustomevents/commands/Eventcommand.java
@@ -1,5 +1,14 @@
 package com.periodicbrake_dev.fullcustomevents.commands;
 
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
 public class Eventcommand {
 
 }
--- src/main/java/com/periodicbrake_dev/fullcustomevents/commands/Questcommand.java
+++ src/main/java/com/periodicbrake_dev/fullcustomevents/commands/Questcommand.java
@@ -1,5 +1,15 @@
-package com.periodicbrake_dev.fullcustomevents;
+package com.periodicbrake_dev.fullcustomevents.commands;
 
-public class commands {
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
+public class Questcommand {
+    @SubscribeEvent
+    
 }
