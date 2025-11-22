df74704401b31610c18c984967a41a2f64f83d08
PeriodicBrake63 <125907242+PeriodicBrake63@users.noreply.github.com>
2025-11-22 19:14:54 +0000

    none

--- src/main/java/com/periodicbrake_dev/fullcustomevents/commands/Eventcommand.java
+++ src/main/java/com/periodicbrake_dev/fullcustomevents/commands/Eventcommand.java
@@ -4,7 +4,6 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
-import net.minecraft.network.chat.TextComponent;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
