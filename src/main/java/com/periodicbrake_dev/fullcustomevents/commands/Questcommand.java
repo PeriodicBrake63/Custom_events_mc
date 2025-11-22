661dfd81fd4dc17bd7ba1e9f1f6359483bbb189d
PeriodicBrake63 <125907242+PeriodicBrake63@users.noreply.github.com>
2025-11-22 19:13:46 +0000

    none

--- src/main/java/com/periodicbrake_dev/fullcustomevents/commands/Questcommand.java
+++ src/main/java/com/periodicbrake_dev/fullcustomevents/commands/Questcommand.java
@@ -2,14 +2,35 @@ package com.periodicbrake_dev.fullcustomevents.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
+
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
-import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.Component;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+@Mod.EventBusSubscriber
 public class Questcommand {
+
     @SubscribeEvent
-    
+    public static void onCommandRegister(RegisterCommandsEvent event){
+        CommandDispatcher<CommandSourceStack> dispatcher = event.getDispatcher();
+
+        dispatcher.register(
+            Commands.literal("quest")
+                .then(
+                    Commands.literal("menu")
+                        .executes(Questcommand::openQuestMenu)
+                )
+        );
+    }
+
+    private static int openQuestMenu(CommandContext<CommandSourceStack> context){
+        context.getSource().sendSuccess(
+            () -> Component.literal("command received: /quest menu"),
+            false
+        );
+        return 1;
+    }
 }
