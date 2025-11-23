713b18802c823e6d222f68ef278a2bee00a585bc
PeriodicBrake63 <125907242+PeriodicBrake63@users.noreply.github.com>
2025-11-23 08:16:24 +0000

    none

--- src/main/java/com/periodicbrake_dev/fullcustomevents/GUI/QuestScreen.java
+++ src/main/java/com/periodicbrake_dev/fullcustomevents/GUI/QuestScreen.java
@@ -1,24 +1,33 @@
 package com.periodicbrake_dev.fullcustomevents.GUI;
 
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 
 public class QuestScreen extends Screen {
 
+    private static final ResourceLocation BG =
+            new ResourceLocation("fullcustomevents", "textures/gui/quests.png");
+
     public QuestScreen() {
-        super(Component.literal("Quest Menu"));
+        super(Component.literal("Quests"));
     }
 
     @Override
     protected void init() {
+        // rien pour l’instant
     }
 
     @Override
-    public void renderBackground() {
-    }
+    public void render(GuiGraphics gui, int mouseX, int mouseY, float partialTick) {
+        this.renderBackground(gui); // fond noir comme l'inventaire
 
-    @Override
-    public boolean isPauseScreen() {
-        return false;
+        int x = (this.width - 192) / 2;  // 192 = largeur de ta texture
+        int y = (this.height - 128) / 2; // 128 = hauteur de ta texture
+
+        gui.blit(BG, x, y, 0, 0, 192, 128); // BAM texture affichée
+
+        super.render(gui, mouseX, mouseY, partialTick);
     }
 }
